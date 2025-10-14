6b6ddc379e796ca573f0b2e1b224ee7b0597be7a
Zorkijvolk <zorkijvolkmainkraft@gmail.com>
2025-10-14 21:32:43 +0300

    first

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication
+from PyQt6.QtGui import QFont
+import sys
 
 
-# Press the green button in the gutter to run the script.
+class MainWindow(QMainWindow):
+    def __init__(self, width, height):
+        super().__init__()
+        self.InintUI(width, height)
+
+    def InintUI(self, width, height):
+        self.kw = 1500 / width
+        self.kh = 750 / height
+        self.setGeometry(0, 0, width, height)
+        self.setFixedSize(width, height)
+        font = QFont()
+        font.setFamily("Comic Sans MS")
+        self.startbutton = QPushButton("Начать просмотр", self)
+        self.startbutton.resize(round(500 * self.kw), round(100 * self.kh))
+        self.startbutton.move(width // 2 - self.startbutton.width() // 2,
+                              round(height * 0.26) - self.startbutton.height() // 2)
+        self.endbutton = QPushButton("Выйти из программы", self)
+        self.endbutton.resize(round(500 * self.kw), round(100 * self.kh))
+        self.endbutton.move(width // 2 - self.startbutton.width() // 2,
+                              round(height * 0.74) - self.startbutton.height() // 2)
+        self.guidebutton = QPushButton("Дополнительная информация", self)
+        self.guidebutton.resize(round(500 * self.kw), round(100 * self.kh))
+        self.guidebutton.move(width // 2 - self.startbutton.width() // 2,
+                            round(height * 0.58) - self.startbutton.height() // 2)
+        self.testbutton = QPushButton("Обучение по работе с программой", self)
+        self.testbutton.resize(round(500 * self.kw), round(100 * self.kh))
+        self.testbutton.move(width // 2 - self.startbutton.width() // 2,
+                              round(height * 0.42) - self.startbutton.height() // 2)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app = QApplication(sys.argv)
+    screen = QApplication.screens()[0].size()
+    window = MainWindow(screen.width(), screen.height())
+    window.show()
+    sys.exit(app.exec())
