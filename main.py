b888652a77f32d05607f2e13eb622330866de4c7
Zorkijvolk <zorkijvolkmainkraft@gmail.com>
2025-10-15 16:25:00 +0300

    zagotovki

--- main.py
+++ main.py
@@ -21,24 +21,27 @@ class MainWindow(QMainWindow):
                               round(height * 0.26) - self.startbutton.height() // 2)
         self.endbutton = QPushButton("Выйти из программы", self)
         self.endbutton.resize(round(500 * self.kw), round(100 * self.kh))
-        self.endbutton.move(width // 2 - self.startbutton.width() // 2,
-                            round(height * 0.74) - self.startbutton.height() // 2)
+        self.endbutton.move(width // 2 - self.endbutton.width() // 2,
+                            round(height * 0.74) - self.endbutton.height() // 2)
         self.guidebutton = QPushButton("Дополнительная информация", self)
         self.guidebutton.resize(round(500 * self.kw), round(100 * self.kh))
-        self.guidebutton.move(width // 2 - self.startbutton.width() // 2,
-                              round(height * 0.58) - self.startbutton.height() // 2)
+        self.guidebutton.move(width // 2 - self.guidebutton.width() // 2,
+                              round(height * 0.58) - self.guidebutton.height() // 2)
         self.guidebutton.clicked.connect(self.guide)
         self.testbutton = QPushButton("Обучение по работе с программой", self)
         self.testbutton.resize(round(500 * self.kw), round(100 * self.kh))
-        self.testbutton.move(width // 2 - self.startbutton.width() // 2,
-                             round(height * 0.42) - self.startbutton.height() // 2)
+        self.testbutton.move(width // 2 - self.testbutton.width() // 2,
+                             round(height * 0.42) - self.testbutton.height() // 2)
         self.endbutton.clicked.connect(self.end)
         self.guidetext = QTextBrowser(self)
         self.guidetext.resize(round(500 * self.kw), (round(height * 0.48)))
-        self.guidetext.move(width // 2 - self.startbutton.width() // 2,
+        self.guidetext.move(width // 2 - self.guidetext.width() // 2,
                             round(height * 0.26) - round(100 * self.kh) // 2)
         self.guidetext.setText("Разрабы Дауны")
         self.guidetext.hide()
+        self.backbutton = QPushButton("Назад", self)
+        self.backbutton.resize(round(500 * self.kw), round(100 * self.kh))
+        self.backbutton.move()
 
     def end(self):
         sys.exit()
@@ -52,6 +55,12 @@ class MainWindow(QMainWindow):
     def guide(self):
         self.hidebuttons()
         self.guidetext.show()
+
+    def test(self):
+        self.hidebuttons()
+
+    def start(self):
+        self.hidebuttons()
 
 
 if __name__ == '__main__':
