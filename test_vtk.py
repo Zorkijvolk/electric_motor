934a20f4ed4a2d92cdd7311bdad8546b338fca6b
Zorkijvolk <zorkijvolkmainkraft@gmail.com>
2025-10-28 21:34:36 +0300

    test_vtk

--- test_vtk.py
+++ test_vtk.py
@@ -1,6 +1,16 @@
-from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication, QTextBrowser
+from PyQt6.QtWidgets import QMainWindow, QPushButton, QApplication, QTextBrowser, QFrame
 from PyQt6.QtGui import QFont
 import sys
+import vtkmodules.vtkInteractionStyle
+import vtkmodules.vtkRenderingOpenGL2
+import vtk
+from vtkmodules.qt.QVTKRenderWindowInteractor import QVTKRenderWindowInteractor
+from vtkmodules.vtkFiltersSources import vtkSphereSource
+from vtkmodules.vtkRenderingCore import (
+    vtkActor,
+    vtkPolyDataMapper,
+    vtkRenderer
+)
 
 
 class MainWindow(QMainWindow):
@@ -15,6 +25,45 @@ class MainWindow(QMainWindow):
         self.setFixedSize(width, height)
         font = QFont()
         font.setFamily("Comic Sans MS")
+
+        ball = vtk.vtkSTLReader()
+        ball.SetFileName("models/ball.stl")
+        cilinder = vtk.vtkSTLReader()
+        cilinder.SetFileName("models/cilinder.stl")
+        cube = vtk.vtkSTLReader()
+        cube.SetFileName("models/cube.stl")
+        self.frame = QFrame()
+        self.vtkWidget = QVTKRenderWindowInteractor(self.frame)
+        self.vtkWidget.resize(width, height)
+        self.ren = vtkRenderer()
+        self.vtkWidget.GetRenderWindow().AddRenderer(self.ren)
+        self.iren = self.vtkWidget.GetRenderWindow().GetInteractor()
+
+        mapper1 = vtkPolyDataMapper()
+        mapper1.SetInputConnection(ball.GetOutputPort())
+        mapper2 = vtkPolyDataMapper()
+        mapper2.SetInputConnection(cube.GetOutputPort())
+        mapper3 = vtkPolyDataMapper()
+        mapper3.SetInputConnection(cilinder.GetOutputPort())
+
+        actor1 = vtkActor()
+        actor1.SetMapper(mapper1)
+#        actor2 = vtkActor()
+#        actor2.SetMapper(mapper2)
+        actor3 = vtkActor()
+        actor3.SetMapper(mapper3)
+
+        self.ren.AddActor(actor1)
+#        self.ren.AddActor(actor2)
+        self.ren.AddActor(actor3)
+
+        self.ren.ResetCamera()
+
+        self.setCentralWidget(self.frame)
+        self.frame.resize(1500, 1500)
+#        self.frame.move(0, 0)
+        self.frame.hide()
+
         self.startbutton = QPushButton("Начать просмотр", self)
         self.startbutton.resize(round(500 * self.kw), round(100 * self.kh))
         self.startbutton.move(width // 2 - self.startbutton.width() // 2,
@@ -33,7 +82,7 @@ class MainWindow(QMainWindow):
         self.testbutton.resize(round(500 * self.kw), round(100 * self.kh))
         self.testbutton.move(width // 2 - self.testbutton.width() // 2,
                              round(height * 0.42) - self.testbutton.height() // 2)
-        self.testbutton.clicked.connect(self.start)
+        self.testbutton.clicked.connect(self.test)
         self.endbutton.clicked.connect(self.end)
         self.guidetext = QTextBrowser(self)
         self.guidetext.resize(round(500 * self.kw), (round(height * 0.48)))
@@ -63,6 +112,7 @@ class MainWindow(QMainWindow):
 
     def test(self):
         self.hidebuttons()
+        self.frame.show()
 
     def start(self):
         self.hidebuttons()
@@ -74,11 +124,14 @@ class MainWindow(QMainWindow):
         self.guidebutton.show()
         self.backbutton.hide()
         self.guidetext.hide()
+        self.frame.hide()
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     screen = QApplication.screens()[0].size()
     window = MainWindow(screen.width(), screen.height())
+    window.show()
     window.showFullScreen()
+    window.iren.Initialize()
     sys.exit(app.exec())
